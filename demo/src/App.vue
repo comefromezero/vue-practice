716e033f39d0a88a15eef185d55511b60ad0a94f
lengye7 <zhangzhuo_ming@163.com>
2021-08-03 19:24:28 +0800

    完成了轮播图的左右箭头部分

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,52 +1,69 @@
 <template>
     <div>
-        <myimage  v-for="item in imglist" :src="item.src" :key="item.src"  :width="item.width" :height="item.height" :fit="item.fit"></myimage>
-        <button @click="ChangeSrc">按钮</button>
+        <div style="border:1px solid #abadb2;padding:4px;">
+            <h3>图片组件示例</h3>
+            <hr>
+            <myimage
+                v-for="item in imglist"
+                :src="item.src"
+                :key="item.src"
+                :width="item.width"
+                :height="item.height"
+                :fit="item.fit"
+            ></myimage>
+            <hr>
+            <button @click="ChangeSrc" type="button">更改src地址，成功加载图片</button>
+        </div>
+        <br>
+        <div>
+            <myimageloop style="width:300px;height:300px;background:red;"></myimageloop>
+        </div>
     </div>
 </template>
 
 <script>
-import myimage from './components/myimage.vue'
+import myimage from "./components/myimage.vue"
+import myimageloop from './components/imageloop.vue'
 export default {
-    name:"App",
-    components:{
+    name: "App",
+    components: {
         myimage,
+        myimageloop,
     },
-    data(){
+    data() {
         return {
-            imglist:[
+            imglist: [
                 {
-                    src:"1",
-                    width:"80px",
-                    height:"100px"
+                    src: "1",
+                    width: "80px",
+                    height: "100px",
                 },
                 {
-                    src:"2",
-                    width:"200px",
-                    height:"100px",
-                    fit:"cover",
+                    src: "2",
+                    width: "200px",
+                    height: "100px",
+                    fit: "cover",
                 },
                 {
-                    src:"3",
-                    width:"200px",
-                    height:"100px"
+                    src: "3",
+                    width: "200px",
+                    height: "100px",
                 },
                 {
-                    src:"4",
-                    width:"200px",
-                    height:"100px"
+                    src: "4",
+                    width: "200px",
+                    height: "100px",
                 },
-            ]
-        }
+            ],
+        };
     },
     methods: {
-        ChangeSrc(){
-            this.imglist[1].src="/test.png"
+        ChangeSrc() {
+            this.imglist[1].src = "/test.png";
         },
     },
-}
+};
 </script>
 
 <style>
-
 </style>
--- demo/src/components/imageloop.vue
+++ demo/src/components/imageloop.vue
@@ -1,16 +1,61 @@
 <template>
-    <div>
+    <div class="imageloop_wrapper">
+        <button class="arrow_ arrow_left">
+            <span class="arrow_left_content"></span>
+        </button>
+
+
+        <button class="arrow_ arrow_right">
+            <span class="arrow_right_content"></span>
+        </button>
+        <myimage  ></myimage>
     </div>
 </template>
 
 <script>
-import image from './myimage.vue'
+import myimage from './myimage.vue'
 export default {
     name:"loop",
-    components:{image,}
+    components:{myimage,},
+    data(){
+        return{
+            curIndex:0,
+        }
+    },
+    props:['imglist'], /* imglist是一个数组，每个item是一个对象：{width,height,position,fit,src} */
+    
 }
 </script>
 
 <style>
-
+.imageloop_wrapper{
+    position:relative;
+}
+.arrow_{
+    position: absolute;
+    top: 50%;
+    transform: translate(0,-50%);
+    z-index: 2022;
+    background-color: rgba(31,45,61,0.11);
+    border: none;
+    padding: 0;
+    margin:0;
+    border-radius: 50%;
+    width: 36px;
+    height: 36px;
+    color: #FFFFFF;
+    cursor: pointer;
+}
+.arrow_left{
+    left: 16px;
+}
+.arrow_left_content::before{
+    content: "<";
+}
+.arrow_right{
+    right: 16px;
+}
+.arrow_right_content::before{
+    content: ">";
+}
 </style>
