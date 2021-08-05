0ecf7475db08e347d08a9163f3bbd78fc04709c5
lengye7 <zhangzhuo_ming@163.com>
2021-08-05 18:10:28 +0800

    这里手动引入第三方库echarts，而不通过npm安装。echarts库房在lib目录下。

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -27,12 +27,14 @@
             <myimageloop style="width:400px;height:300px;" type="link-pic" interVal="4000" :itemlist="itemlist"></myimageloop>
             <hr>
         </div>
+        <div id="main" style="width: 600px;height:400px;"></div>
     </div>
 </template>
 
 <script>
 import myimage from "./components/myimage.vue"
 import myimageloop from './components/imageloop.vue'
+import * as echarts from '../lib/echarts-5.1.2/dist/echarts.min'
 export default {
     name: "App",
     components: {
@@ -97,6 +99,33 @@ export default {
         ChangeSrc() {
             this.imglist[1].src = "/test.png";
         },
+    },
+    mounted(){
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = echarts.init(document.getElementById('main'));
+
+        // 指定图表的配置项和数据
+        let option = {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
     },
 };
 </script>
